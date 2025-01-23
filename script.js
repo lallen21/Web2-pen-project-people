0a1ad49a8b47d72967adfbd7369f51c7c94365a3
lallen21 <156361246+lallen21@users.noreply.github.com>
2025-01-23 19:50:39 +0000

    Added *confetti *new favicons *js script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Okay👍');
+
+// Built in 
+
+// Source for library
+// https://confettijs.org/confetti.min.js
+
+// Create varibles, instantiate 'new' instance of 'confetti'
+
+let confetti = new Confetti("confetti");
+
+// Options 
+confetti.destroyTarget(false);
+confetti.setSize(10);
+confetti.setPower(50);
+confetti.setCount(500)
+
+// console.log(confetti);
